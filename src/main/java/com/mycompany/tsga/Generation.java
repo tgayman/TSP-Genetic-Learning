0f0ffc1e3692e5c2217bdabd9aa075c6d6b3551f
tgayman <tgayman@purdue.edu>
2020-09-11 13:48:45 -0400

    Update Generation.java

--- src/main/java/com/mycompany/tsga/Generation.java
+++ src/main/java/com/mycompany/tsga/Generation.java
@@ -1,46 +1,69 @@
-/*
- */
 package com.mycompany.tsga;
 
 import java.util.Arrays;
+import java.awt.Point;
 
 /**
  *
- * @author troyg
+ * @author Troy Gayman
  */
 public class Generation {
 
-    private Individual[] generation;
-
-    public Generation(Individual[] genIn) {//Creates a generation zero of individuals with random genomes
-        generation = genIn.clone();
-        Arrays.sort(generation);
+    private Individual[] population;
+    private int populationSize;
+    
+    /*
+        Constructor used to create an initial generation populated with random paths
+    */
+    public Generation(Point[] cities, int populationSize) {
+        this.populationSize = populationSize;
+        this.population = fillPopulation(cities);
+        Arrays.sort(this.population);
+    }
+    
+    /*
+        Constructor used to create an evolved generation that accepts a premade population
+    */
+    public Generation(Individual[] population, int populationSize) {
+        this.populationSize = populationSize;
+        this.population = population;
+        Arrays.sort(this.population);
     }
 
-    public Individual[] getGenArray() {
-        return generation;
+    /*
+        Creates a population of individuals to fill this generation
+    */
+    private Individual[] fillPopulation(Point[] cities) {
+        Individual[] randGen = new Individual[populationSize];
+        for (int i = 0; i < populationSize; i++) {
+            randGen[i] = new Individual(cities, true);
+        }
+        return randGen;
     }
 
+    /*----------------Get/Set/Print methods below------------------*/
 
     public void printGen() {
         System.out.println("printing generation");
-        for (Individual i : generation) {
+        for (Individual i : population) {
             i.printIndv();
         }
         System.out.println("");
     }
 
-
-    //mutates evathang
-    public Generation evolve(){
-        Individual[] newGen = generation.clone();
-        for(int i = 0; i < newGen.length; i++){
-            newGen[i] = generation[i].mutate();
-        }
-        return new Generation(newGen);
+    public Individual[] getPopulation() {
+        return population;
     }
     
-    public Individual getTop(){
-        return generation[0];
+    public Individual getBest() {
+        return population[0];
+    }
+
+    public Individual getMedian() {
+        return population[(int) (populationSize * 0.5)];
+    }
+
+    public Individual getWorst() {
+        return population[populationSize - 1];
     }
 }
