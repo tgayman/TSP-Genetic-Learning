41ceeaf622d249ddf159de3e991b95bcb3eeab47
tgayman <tgayman@purdue.edu>
2020-09-11 13:45:53 -0400

    Update UIController.java

--- src/main/java/com/mycompany/tsga/UIController.java
+++ src/main/java/com/mycompany/tsga/UIController.java
@@ -1,202 +1,33 @@
 package com.mycompany.tsga;
 
-import java.awt.Point;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Random;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  *
  * @author Troy Gayman
  */
-public class SimulationController {
-
-    private final int pathSize = 25;
-    private final int gridX = 15;
-    private final int gridY = 10;
-    private final int populationSize = 1000;
-    private int generationNumber;
-    private Generation generation;
-    private Point[] cities;
-    private Individual greedyPath;
+public class UIController {
 
     /*
-        Constructor which initializes the simulation, including generation 0
+        Constructor which initializes the GUI frame and adds the main panel
      */
-    public SimulationController() {
-        this.cities = initCities().clone();
-        this.generation = new Generation(cities, populationSize);
-        generationNumber = 0;
-        initGreedyPath();
+    public UIController(SimulationController sim) {
+        JFrame frame = createJFrame();
+        JPanel mainP = new MainPanel(sim);
+        frame.add(mainP);
+        frame.setVisible(true);
     }
 
     /*
-        Method to reset the simulation in the GUI, starts again
-        with new nodes and generation 0
+        Helper method for the constructor. Creates the JFrame used for the 
+        GUI
      */
-    public void reset() {
-        this.cities = initCities().clone();
-        this.generation = new Generation(cities, populationSize);
-        generationNumber = 0;
-        initGreedyPath();
-    }
-
-    /*
-        A method to evolve the current generation, only utalizes mutation, or swapping
-        rather than genome crossing which is not implemented here.
-        It uses both mutateMajor(), and mutateMinor()
-        Adjustments are made after generation 75 because drastic mutations are
-        desireable in the beginning of the expirament, and smaller changes are 
-        better later on.
-     */
-    public void swapEvolve() {
-        Individual[] evolvedPopulation = generation.getPopulation().clone();
-        int highPopStrata;
-        if (generationNumber > 75) {
-            highPopStrata = Math.max(2, (int) (populationSize * 0.1));
-        } else {
-            highPopStrata = Math.max(2, (int) (populationSize * 0.3));
-        }
-        Random r = new Random();
-        for (int i = highPopStrata; i < populationSize; i++) {
-            if (r.nextBoolean() || generationNumber > 75) {
-                evolvedPopulation[i] = mutateMinor(evolvedPopulation[i % highPopStrata]);
-            } else {
-                evolvedPopulation[i] = mutateMajor(evolvedPopulation[i % highPopStrata]);
-            }
-        }
-
-        Generation evolvedGeneration = new Generation(evolvedPopulation, populationSize);
-        this.generation = evolvedGeneration;
-        this.generationNumber++;
-    }
-
-    /*
-        Initializes the individual greedyPath which contains the greedy algorithm path
-        and distance
-     */
-    private void initGreedyPath() {
-        Point[] greedy = new Point[pathSize];
-        ArrayList<Point> pathList = new ArrayList();
-        Collections.addAll(pathList, cities);
-        greedy[0] = pathList.get(0);
-        pathList.remove(0);
-        for (int i = 1; i < greedy.length; i++) {
-            greedy[i] = getClosestPoint(greedy[i - 1], pathList);
-            pathList.remove(greedy[i]);
-        }
-        this.greedyPath = new Individual(greedy, false);
-    }
-
-    /*
-        A helper method for initGreedyPath(), which determines the closest
-        point from pointList, and point p
-     */
-    public Point getClosestPoint(Point p, ArrayList<Point> pointList) {
-        double minDist = 100;
-        Point ret = null;
-        for (Point p2 : pointList) {
-            double deltaX = Math.abs(p.x - p2.x);
-            double deltaY = Math.abs(p.y - p2.y);
-            double dist = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
-            if (dist < minDist) {
-                ret = p2;
-                minDist = dist;
-            }
-        }
-        return ret;
-    }
-
-    /*
-        A mutation method which only swaps two points in the genome
-     */
-    private Individual mutateMinor(Individual i) {
-        Point[] path = i.getPath().clone();
-        Random r = new Random();
-        int index = r.nextInt(path.length - 1) + 1;
-        int index2 = r.nextInt(path.length - 1) + 1;
-        Point tmp = path[index];
-        path[index] = path[index2];
-        path[index2] = tmp;
-        return new Individual(path, false);
-    }
-
-    /*
-        A mutation method which makes a swap for 10% of the nodes in the path
-     */
-    private Individual mutateMajor(Individual i) {
-        Point[] path = i.getPath().clone();
-        int gamma = Math.max(5, (int) (path.length * 0.1));
-        for (int mutateCount = 0; mutateCount < gamma; mutateCount++) {
-            Random r = new Random();
-            int index = r.nextInt(path.length - 1) + 1;
-            int index2 = r.nextInt(path.length - 1) + 1;
-            Point tmp = path[index];
-            path[index] = path[index2];
-            path[index2] = tmp;
-        }
-        return new Individual(path, false);
-    }
-
-    /*
-        Helper method for the constructor. It creates the base nodes in the 
-        TSP problem, all of the nodes must be visited, starting and ending at the first
-        node
-     */
-    public Point[] initCities() {
-        Point[] tmpPath = new Point[pathSize];
-        Random r = new Random();
-
-        for (int i = 0; i < pathSize; i++) {
-            Point p = new Point(r.nextInt(gridX), r.nextInt(gridY));
-
-            while (!isNewPoint(tmpPath, p)) { //ensure new points are unique
-                p = new Point(r.nextInt(gridX), r.nextInt(gridY));
-            }
-
-            tmpPath[i] = p;
-        }
-        return tmpPath;
-    }
-
-    /*
-        A helper method for initCities(). It ensures that there are
-        no duplicated nodes
-     */
-    public boolean isNewPoint(Point[] tmpPath, Point p) { // check if Point p is already in Point[] path
-        boolean isNewPoint = true;
-        for (int i = 0; i < tmpPath.length; i++) {
-            if (tmpPath[i] != null) {
-                if (tmpPath[i].equals(p)) {
-                    return false;
-                }
-            }
-        }
-        return isNewPoint;
-    }
-
-    /*-------------------------- Print/Get/Set Methods Below ----------------------*/
-    public Point[] getCities() {
-        return cities;
-    }
-
-    public Individual getGreedy() {
-        return greedyPath;
-    }
-
-    public int getGridX() {
-        return gridX;
-    }
-
-    public int getGridY() {
-        return gridY;
-    }
-
-    public int getGenerationNumber() {
-        return generationNumber;
-    }
-
-    public Generation getGen() {
-        return generation;
+    public JFrame createJFrame() {
+        JFrame frame = new JFrame("TSP");
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        return frame;
     }
 }
